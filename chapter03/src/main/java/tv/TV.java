c6fedee3885cd2073be6da16a807fb51cf4f077f
yusubhin <sdbtnqlss@gmail.com>
2024-04-25 11:12:26 +0900

    TV #1

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,41 @@
+package tv;
+
+public class TV {
+	private int volume;		// 0 ~ 10
+	private int channel;	// 1 ~ 255
+	private boolean power;
+	
+	public void status() {
+		System.out.println("TV[power=off, channel,=10, volume=100]");
+	}
+	
+	public TV(int i, int j, boolean b) {
+		
+	}
+
+	public void power(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void volume(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void volume(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void channel(boolean i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void channel(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
