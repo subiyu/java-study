ed55d8722cb77cbff58dad7300eb361892b16758
yusubhin <sdbtnqlss@gmail.com>
2024-04-29 13:47:46 +0900

    Practice #Date

---
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class CalendarTest {
+
+	public static void main(String[] args) {
+		Calendar.getInstance();
+		
+		// Locale aLocale = Locale.getDefault(Locale.Category.FORMAT);
+		// TimeZone tz = TimeZone.getDefault();
+		// System.out.println(aLocale + ":" + tz);
+		
+		System.out.println();
+	}
+}
