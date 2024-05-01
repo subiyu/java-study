a9b0627a7205ca5f6b26fec12c47e66166af81f8
yusubhin <sdbtnqlss@gmail.com>
2024-05-01 15:37:01 +0900

    Practice #TCP - 2

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -21,7 +24,50 @@ public class TCPServer {
 			// 3. accept
 			Socket socket = serverSocket.accept(); //blocking
 			
-			System.out.println("연결!!!");
+			
+			/* data socket */
+			try {				
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
+				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+				int remotePort = inetRemoteSocketAddress.getPort();
+				System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+				
+				//System.out.println("연결!!!");
+				//4. IO Stream 받아오기
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				while(true) {
+					// 5. 데이터 읽기
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer); //blocking					
+					
+					if(readByteCount == -1) {
+						//클라이언트가 정상적으로 종료(close() 호출)
+						System.out.println("[server] closed by client");
+						break;
+					}
+					
+					String data = new String(buffer, 0, readByteCount, "UTF-8");
+					System.out.println("[server] received: " + data);
+
+					// 6. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
+				}
+				
+			} catch (SocketException e) {
+				System.out.println("[server] suddenly closed by client");
+			} catch (IOException e) {
+				System.out.println("[server] error: " + e);
+			} finally {
+				try {
+					if(socket != null && !socket.isClosed()) {						
+						socket.close();
+					}
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
 		} catch (IOException e) {
 			System.out.println("[server] error: " + e);
 		} finally {
